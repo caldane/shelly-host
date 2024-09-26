5fab2562a66b823f8738d759f8bd69ef2e1ccccc
Caleb Haldane <caldane@gmail.com>
2024-09-25 19:50:04 -0500

    Add new channels

--- src/utils/channel.helper.ts
+++ src/utils/channel.helper.ts
@@ -1,4 +1,11 @@
 export const channelDictionary = {
     "downstairs": "shellyplus1-bighouse/command/switch:0",
-    "stairs": "shelly-buffington/command/switch:0"
+    "stairs": "shelly-buffington-stairs/command/switch:0",
+    "closet": "shelly-buffington-closet/command/switch:0",
+    "kitchen": "shelly-buffington-kitchen/command/switch:0",
+    "livingroom": "shelly-buffington-livingroom/command/switch:0",
+    "diningroom": "shelly-buffington-diningroom/command/switch:0",
+    "jackAndJill": "shelly-buffington-jackAndJill/command/switch:0",
+    "masterBedroom": "shelly-buffington-masterBedroom/command/switch:0",
+    "masterBathroom": "shelly-buffington-masterBathroom/command/switch:0",
   } as const;
