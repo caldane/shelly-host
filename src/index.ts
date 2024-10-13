3948fab088b8a04bc3f541761c0a00daee4df189
Caleb Haldane <Caleb.Haldane@loves.com>
2024-10-13 15:17:53 -0500

    Move env loading up and add log for env

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import {channelDictionary} from "./utils/channel.helper"
 
 import expressWinston from "express-winston";
 import { logger } from "./logger";
+dotenv.config();
+
+console.log(`[server]: Environment: ${process.env.NODE_ENV}`, { PORT: process.env.PORT, MQTT_URL: process.env.MQTT_URL });
 
 let client = mqtt.connect(process.env.MQTT_URL as string); 
 console.log(`[server]: MQTT Server is running at ${process.env.MQTT_URL}`);
 
 
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
